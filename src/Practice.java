1504922e4a59cdf70c172646909c5fdd4267db01
kranthi <kranthiteja222@gmail.com>
2025-11-14 11:04:01 +0530

    my proj changes

---
+++ src/Practice.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+        Scanner sc=new Scanner(System.in);
+       int k= sc.nextInt();
+       int j=0;
+        for(int i=1;i<=k;i++)
+        {
+          System.out.println(i);
+          j=j+i;
+        }
+        System.out.println(j);
+    }
+
+}
