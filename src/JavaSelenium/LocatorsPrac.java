b8fa7117490a547800d6a2bcd81782ed847da31f
kranthi <kranthiteja222@gmail.com>
2025-11-26 21:23:20 +0530

    LocatorPrac

--- src/JavaSelenium/LocatorsPrac.java
+++ src/JavaSelenium/LocatorsPrac.java
@@ -1,14 +1,29 @@
 package JavaSelenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class LocatorsPrac {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver= new ChromeDriver();
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
-        driver.close();
-    }
+        System.out.println(driver.getTitle());
+        WebElement element = driver.findElement(By.className("gLFyf"));
+        element.sendKeys("amazon.com");
+        Actions action;
+        action=new Actions(driver);
+        action.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000);
+        driver.findElement(By.className("recaptcha-checkbox-border")).click();
 
+
+        driver.close();
+
+    }
 }
